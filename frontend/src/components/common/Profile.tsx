649ecea92f9a036d0fa1913133c94b740ad6483b
Mjdelima20 <markjason.delima@sun-asterisk.com>
2023-11-22 14:40:20 +0800

    SPH-ELS-MARK-9 arrange the code

--- frontend/src/components/common/Profile.tsx
+++ frontend/src/components/common/Profile.tsx
@@ -1,5 +1,5 @@
-import UserProfile from '../user.png'
-import users from '../../dummydata'; 
+import UserProfile from "../user.png";
+import users from "../../dummydata";
 
 interface Users {
   id: number;
@@ -13,20 +13,31 @@ interface Users {
 
 const Profile = () => {
   return (
-    <div className='p-[4rem] ml-[5rem] flex flex-col-2 items-center'>
-    <div>
-    <h1 className='p-[1rem] text-[26px] font-bold tracking-normal'>Dashboard</h1>
-      <img src={UserProfile} alt="profile" className='h-[150px] bg-gray-200 p-2 rounded'/>
+    <div className="p-[4rem] ml-[5rem] flex flex-col-2 items-center">
+      <div>
+        <h1 className="p-[1rem] text-[26px] font-bold tracking-normal">
+          Dashboard
+        </h1>
+        <img
+          src={UserProfile}
+          alt="profile"
+          className="h-[150px] bg-gray-200 p-2 rounded"
+        />
+      </div>
+      {users.slice(1, 2).map((user: Users, id: number) => (
+        <div className="px-8 mt-9" key={id}>
+          <h1 className="font-semibold text-[21px]">
+            {user.first_name} {user.last_name}
+          </h1>
+          <p className="underline text-blue-400 p-1">
+            Learned {user.score} words
+          </p>
+          <p className="underline text-blue-400 p-1">
+            Learned {user.taken_category} lessons
+          </p>
+        </div>
+      ))}
     </div>
-     {users.slice(1,2).map((user:Users , index:number) => (
-              <div className='px-8 mt-9' key={index}>
-              <h1 className='font-semibold text-[21px]'>{user.first_name} {user.last_name}</h1>
-              <p className='underline text-blue-400 p-1'>Learned {user.score} words</p>
-              <p className='underline text-blue-400 p-1'>Learned {user.taken_category} lessons</p>
-            </div>
-         
-     ))}
-   </div>
   );
-}
+};
 export default Profile;
