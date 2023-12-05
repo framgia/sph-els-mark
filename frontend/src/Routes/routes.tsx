60dba0de7e067da46416c28370a6e4c04db5a464
Miguel <miguelbrando.monacillo@sun-asterisk.com>
2023-12-05 11:48:24 +0800

    Create Admin | Add Category page

--- frontend/src/Routes/routes.tsx
+++ frontend/src/Routes/routes.tsx
@@ -6,6 +6,7 @@ import SignUp from "../Pages/auth/SignupPage";
 import AdminLogin from "../Pages/auth/AdminLogin";
 import AdminSignin from "../Pages/auth/AdminSignUp";
 import Dashboard from "../Pages/Dashboard";
+import AddCategoryPage from "../Pages/AddCategoryPage";
 
 
 const Main = () => {
@@ -18,6 +19,7 @@ const Main = () => {
       <Route path="/admin-login" element={<AdminLogin/>} />
       <Route path="/admin-register" element={<AdminSignin/>} />
       <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/admin/category/addcategory" element={<AddCategoryPage/>} />
     </Routes>
     
   )
--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+
+interface NavbarProps{
+  title: string;
+}
+
+const Navbar:React.FC<NavbarProps> = ({title}) => {
   return (
    <div className="flex flex-col-2 bg-primary-600 px-9 py-5 items-baseline justify-between text-white">
     <div className="p-2 pl-[5rem]">
-      <h1 className="font-bold text-[26px] "><Link to={'/dashboard'}>E-learning System</Link></h1>
+      <h1 className="font-bold text-[26px] "><Link to={'/dashboard'}>{title}</Link></h1>
     </div>
     <div className=" flex ">
       <ul className="flex p-2 text-[18px]">
