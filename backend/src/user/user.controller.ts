07a4c5b7aa3f3919c0442cb44590363a743f646d
John Russel Fontillas <142206621+Fontilllas@users.noreply.github.com>
2024-01-09 16:21:03 +0800

    [SELS-TASK] [BE]-Admin - Display student users list (#23)
    * fix conflict
    
    * fixed a comment

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  NotFoundException,
+  Req,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   @Get('/admin')
   async admin() {
@@ -11,5 +23,23 @@ export class UserController {
         is_admin: true,
       },
     });
+  }
+  @UseGuards(AuthGuard)
+  @Get('admin/user-lists')
+  async getRegisteredStudents(@Req() request: Request) {
+    const cookie = request.cookies['jwt'];
+    const { id: user_id } = await this.jwtService.verifyAsync(cookie);
+    const user = this.userService.findOne({ where: { user_id } });
+    const admin = await this.userService.findOne({
+      where: { is_admin: false },
+    });
+
+    if (!admin && !user) {
+      throw new NotFoundException('Forbidden Resource');
+    } 
+    const students = await this.userService.find({
+      where: { is_admin: false },
+    });
+    return students;
   }
 }
